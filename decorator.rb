520276b857cae55c4a2117c316fb630a6322b61c
Alabi12 <wolo22001@gmail.com>
2023-02-07 17:57:50 +0000

    update the decorator file

--- decorator.rb
+++ decorator.rb
@@ -1,8 +1,11 @@
 require_relative './nameable'
 class Decorator < Nameable
+  attr_accessor :nameable
+  
   def initialize(nameable)
     super()
     @nameable = nameable
+    @attr_accessor
   end
 
   def correct_name
